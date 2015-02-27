47220f7732e846a4c5a29167c0caa76794dffdfb
Matt Malinao <matt.malinao@gmail.com>
2015-02-26 20:24:40 -0700

    NbaTeam seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+NbaTeam.where(uid: 'ATL').first_or_create
+NbaTeam.where(uid: 'BKN').first_or_create
+NbaTeam.where(uid: 'BOS').first_or_create
+NbaTeam.where(uid: 'CHA').first_or_create
+NbaTeam.where(uid: 'CHI').first_or_create
+NbaTeam.where(uid: 'CLE').first_or_create
+NbaTeam.where(uid: 'DAL').first_or_create
+NbaTeam.where(uid: 'DEN').first_or_create
+NbaTeam.where(uid: 'DET').first_or_create
+NbaTeam.where(uid: 'GSW').first_or_create
+NbaTeam.where(uid: 'HOU').first_or_create
+NbaTeam.where(uid: 'IND').first_or_create
+NbaTeam.where(uid: 'LAC').first_or_create
+NbaTeam.where(uid: 'LAL').first_or_create
+NbaTeam.where(uid: 'MEM').first_or_create
+NbaTeam.where(uid: 'MIA').first_or_create
+NbaTeam.where(uid: 'MIL').first_or_create
+NbaTeam.where(uid: 'MIN').first_or_create
+NbaTeam.where(uid: 'NOP').first_or_create
+NbaTeam.where(uid: 'NYK').first_or_create
+NbaTeam.where(uid: 'OKC').first_or_create
+NbaTeam.where(uid: 'ORL').first_or_create
+NbaTeam.where(uid: 'PHI').first_or_create
+NbaTeam.where(uid: 'PHX').first_or_create
+NbaTeam.where(uid: 'POR').first_or_create
+NbaTeam.where(uid: 'SAC').first_or_create
+NbaTeam.where(uid: 'SAS').first_or_create
+NbaTeam.where(uid: 'TOR').first_or_create
+NbaTeam.where(uid: 'UTA').first_or_create
+NbaTeam.where(uid: 'WAS').first_or_create
+
+
+
+
+

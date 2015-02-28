a9c3b86c9098bd02b473e23e89d1ff910b7e310f
Matt Malinao <matt.malinao@gmail.com>
2015-02-28 10:48:03 -0700

    SlackTeam#authenticate_with_token

--- app/models/slack_team.rb
+++ app/models/slack_team.rb
@@ -3,4 +3,9 @@ class SlackTeam < ActiveRecord::Base
   has_many :slack_channels
 
   validates :slack_id, :token, presence: true
+
+  def self.authenticate_with_token(slack_id, token)
+    team = SlackTeam.find_by_slack_id(slack_id)
+    team && Devise.secure_compare(team.token, token) ? team : nil
+  end
 end
--- spec/models/slack_team_spec.rb
+++ spec/models/slack_team_spec.rb
@@ -6,4 +6,26 @@ RSpec.describe SlackTeam, type: :model do
 
   it { is_expected.to validate_presence_of :slack_id }
   it { is_expected.to validate_presence_of :token }
+
+  describe '.authenticate_with_token' do
+    subject { SlackTeam.authenticate_with_token(slack_id, token) }
+
+    let!(:slack_team) { FactoryGirl.create(:slack_team) }
+    let(:slack_id) { slack_team.slack_id }
+    let(:token) { slack_team.token }
+
+    it 'should return existing slack team' do
+      is_expected.to eq slack_team
+    end
+
+    context 'when slack team does not exist with given slack_id' do
+      let(:slack_id) { 'foo' }
+      it { is_expected.to be_nil }
+    end
+
+    context 'when slack team does not exist with given token' do
+      let(:token) { 'bar' }
+      it { is_expected.to be_nil }
+    end
+  end
 end

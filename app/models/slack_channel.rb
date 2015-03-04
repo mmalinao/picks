d5169defac43668f92f3aa64bc8e1de768a74a4f
Matt Malinao <matt.malinao@gmail.com>
2015-03-03 22:51:31 -0700

    clean up unused old code

--- app/models/slack_channel.rb
+++ app/models/slack_channel.rb
@@ -8,15 +8,6 @@ class SlackChannel < ActiveRecord::Base
 
   enum sports_type: [:hoops, :pucks, :pigskins, :futbol, :march_madness]
 
-  def self.sports_channel_for(slack_team, slack_id, sports_type)
-    channel = SlackChannel.find_by_slack_id(slack_id)
-    channel.nil? ? create(slack_team: slack_team, slack_id: slack_id, sports_type: sports_type) : channel
-  end
-
-  def create_associated_slack_user
-    # TODO: need to create join model plus the slack_ids...
-  end
-
   def first_slack_user_or_create(sid, name)
     user = slack_users.find_by_sid(sid)
 
--- spec/models/slack_channel_spec.rb
+++ spec/models/slack_channel_spec.rb
@@ -8,51 +8,6 @@ RSpec.describe SlackChannel, type: :model do
   it { is_expected.to validate_presence_of :slack_team }
   it { is_expected.to validate_presence_of :sports_type }
 
-  xdescribe '.sports_channel_for' do
-    subject(:do_action) { SlackChannel.sports_channel_for(slack_team, slack_id, sports_type) }
-
-    let!(:slack_team) { FactoryGirl.create(:slack_team) }
-    let(:slack_id) { 'C111' }
-    let(:sports_type) { 'hoops' }
-    let(:new_channel) { SlackChannel.last }
-
-    it 'should create a new SlackChannel' do
-      expect { do_action }.to change { SlackChannel.count }.by(1)
-    end
-
-    it 'should create a new SlackChannel with chanel id' do
-      do_action
-      expect(new_channel.slack_id).to eq slack_id
-    end
-
-    it 'should create a new SlackChannel with sports_type' do
-      do_action
-      expect(new_channel.sports_type).to eq sports_type
-    end
-
-    it 'should create a new SlackChannel associated with slack team' do
-      do_action
-      expect(new_channel.slack_team).to eq slack_team
-    end
-
-    it 'should return a SlackChannel' do
-      expect(do_action).to be_kind_of SlackChannel
-    end
-
-    context 'when SlackChannel already exists' do
-      let!(:slack_channel) { FactoryGirl.create(:slack_channel, :hoops, slack_team: slack_team) }
-      let(:slack_id) { slack_channel.slack_id }
-
-      it 'should return existing SlackChannel' do
-        expect(do_action).to eq slack_channel
-      end
-
-      it 'should not create a new SlackChannel' do
-        expect { do_action }.to_not change { SlackChannel.count }
-      end
-    end
-  end
-
   describe '#first_slack_user_or_create' do
     subject(:do_action) { channel.first_slack_user_or_create(user.sid, user.name) }
 

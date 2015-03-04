e26d207cf2948e2bf2b935d51ab05748583dcd49
Matt Malinao <matt.malinao@gmail.com>
2015-03-03 21:34:06 -0700

    Pick#parse_command; parse the sports team uid and points from command text

--- app/models/pick.rb
+++ app/models/pick.rb
@@ -3,4 +3,13 @@ class Pick < ActiveRecord::Base
   belongs_to :sports_team
 
   validates :slack_user, :sports_team, :wins_by, presence: true
+
+  def self.create_from_command(command)
+  end
+
+  private
+
+  def parse_command(text)
+    /^(?<sports_team_uid>\D{3}) by (?<points>\d+)$/.match(text)
+  end
 end
--- spec/models/pick_spec.rb
+++ spec/models/pick_spec.rb
@@ -7,4 +7,30 @@ RSpec.describe Pick, type: :model do
   it { is_expected.to validate_presence_of :slack_user }
   it { is_expected.to validate_presence_of :sports_team }
   it { is_expected.to validate_presence_of :wins_by }
+
+  describe '#parse_command' do
+    subject(:match_data) { pick.send(:parse_command, "#{sports_team_uid} by #{points}") }
+
+    let(:pick) { FactoryGirl.create(:pick) }
+    let(:sports_team_uid) { 'LAC' }
+    let(:points) { 10 }
+
+    it 'should parse sports team uid' do
+      expect(match_data[:sports_team_uid]).to eq sports_team_uid
+    end
+
+    it 'should parse points' do
+      expect(match_data[:points].to_i).to eq points
+    end
+
+    context 'when invalid sports team uid' do
+      let(:sports_team_uid) { 'LA' }
+      it { is_expected.to be_nil }
+    end
+
+    context 'when invalid points' do
+      let(:points) { 'b' }
+      it { is_expected.to be_nil }
+    end
+  end
 end

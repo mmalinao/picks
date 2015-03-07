61b95486a4545e7d1153d8aa7b01fee12b261208
Matt Malinao <matt.malinao@gmail.com>
2015-03-06 21:47:55 -0700

    SportsTeam#playing_today?; returns true if team has a home/road game scheduled today

--- app/models/sports_teams/sports_team.rb
+++ app/models/sports_teams/sports_team.rb
@@ -3,4 +3,8 @@ class SportsTeam < ActiveRecord::Base
   has_many :road_games, foreign_key: :away_team_id, class_name: Game
 
   validates :uid, presence: true # uniqueness: true
+
+  def playing_today?
+    home_games.scheduled_today.count > 0 || road_games.scheduled_today.count > 0
+  end
 end
--- spec/models/sports_teams/sports_team_spec.rb
+++ spec/models/sports_teams/sports_team_spec.rb
@@ -6,4 +6,25 @@ RSpec.describe SportsTeam, type: :model do
 
   it { is_expected.to validate_presence_of :uid }
   # it { is_expected.to validate_uniqueness_of :uid }
+
+  describe '#playing_today?' do
+    subject { sports_team.playing_today? }
+
+    before(:each) { allow(Date).to receive(:today).and_return(Date.new(2015, 1, 1)) }
+    let(:sports_team) { FactoryGirl.create(:nba_team) }
+
+    context 'when has a home game scheduled today' do
+      let!(:game) { FactoryGirl.create(:game, home_team: sports_team, schedule: DateTime.new(2015, 1, 1, 18)) }
+      it { is_expected.to be_truthy }
+    end
+
+    context 'when has a road game scheduled today' do
+      let!(:game) { FactoryGirl.create(:game, away_team: sports_team, schedule: DateTime.new(2015, 1, 1, 18)) }
+      it { is_expected.to be_truthy }
+    end
+
+    context 'when has no games scheduled today' do
+      it { is_expected.to be_falsey }
+    end
+  end
 end
